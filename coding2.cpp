899b1bb5df4d8ac9b368392cdeb86c58eef6074f
YeRim Kim <angeyerim@naver.com>
2022-06-28 01:18:37 +0900

    coding2 re

--- coding2.cpp
+++ coding2.cpp
@@ -6,8 +6,8 @@ using namespace std;
 //[카카오인턴] 키패드 누르기 level1
 string solution(vector<int> numbers, string hand) {
     string answer = "";
-    int l_pos = 0;
-    int r_pos = 0;
+    int l_pos = 10;
+    int r_pos = 11;
     int l_dis = 0;
     int r_dis = 0;
     
@@ -22,20 +22,20 @@ string solution(vector<int> numbers, string hand) {
         }
         else if(*iter == 2){
             if(l_pos == 1) l_dis = 1;
-            if(l_pos == 2) l_dis = 0;
-            if(l_pos == 4) l_dis = 2;
-            if(l_pos == 5) l_dis = 1;
-            if(l_pos == 7) l_dis = 3;
-            if(l_pos == 8) l_dis = 2;
-            if(l_pos == 0) l_dis = 3;
+            else if(l_pos == 2) l_dis = 0;
+            else if(l_pos == 4) l_dis = 2;
+            else if(l_pos == 5) l_dis = 1;
+            else if(l_pos == 7) l_dis = 3;
+            else if(l_pos == 8) l_dis = 2;
+            else if(l_pos == 0) l_dis = 3;
             
             if(r_pos == 3) r_dis = 1;
-            if(r_pos == 6) r_dis = 2;
-            if(r_pos == 9) r_dis = 3;
-            if(r_pos == 2) r_dis = 0;
-            if(r_pos == 5) r_dis = 1;
-            if(r_pos == 8) r_dis = 2;
-            if(r_pos == 0) r_dis = 3;
+            else if(r_pos == 6) r_dis = 2;
+            else if(r_pos == 9) r_dis = 3;
+            else if(r_pos == 2) r_dis = 0;
+            else if(r_pos == 5) r_dis = 1;
+            else if(r_pos == 8) r_dis = 2;
+            else if(r_pos == 0) r_dis = 3;
             
             
             if(l_dis > r_dis) {answer.append("R"); r_pos = *iter;}
@@ -47,20 +47,20 @@ string solution(vector<int> numbers, string hand) {
         }
         else if(*iter == 5){
             if(l_pos == 1) l_dis = 2;
-            if(l_pos == 4) l_dis = 1;
-            if(l_pos == 7) l_dis = 2;
-            if(l_pos == 2) l_dis = 1;
-            if(l_pos == 5) l_dis = 0;
-            if(l_pos == 8) l_dis = 1;
-            if(l_pos == 0) l_dis = 2;
+            else if(l_pos == 4) l_dis = 1;
+            else if(l_pos == 7) l_dis = 2;
+            else if(l_pos == 2) l_dis = 1;
+            else if(l_pos == 5) l_dis = 0;
+            else if(l_pos == 8) l_dis = 1;
+            else if(l_pos == 0) l_dis = 2;
             
             if(r_pos == 3) r_dis = 2;
-            if(r_pos == 6) r_dis = 1;
-            if(r_pos == 9) r_dis = 2;
-            if(r_pos == 2) r_dis = 1;
-            if(r_pos == 5) r_dis = 0;
-            if(r_pos == 8) r_dis = 2;
-            if(r_pos == 0) r_dis = 3;
+            else if(r_pos == 6) r_dis = 1;
+            else if(r_pos == 9) r_dis = 2;
+            else if(r_pos == 2) r_dis = 1;
+            else if(r_pos == 5) r_dis = 0;
+            else if(r_pos == 8) r_dis = 2;
+            else if(r_pos == 0) r_dis = 3;
             
             if(l_dis > r_dis) {answer.append("R"); r_pos = *iter;}
             else if(l_dis < r_dis) {answer.append("L"); l_pos = *iter;}
@@ -71,20 +71,20 @@ string solution(vector<int> numbers, string hand) {
         }
         else if(*iter == 8){
             if(l_pos == 1) l_dis = 3;
-            if(l_pos == 4) l_dis = 2;
-            if(l_pos == 7) l_dis = 1;
-            if(l_pos == 2) l_dis = 2;
-            if(l_pos == 5) l_dis = 1;
-            if(l_pos == 8) l_dis = 0;
-            if(l_pos == 0) l_dis = 1;
+            else if(l_pos == 4) l_dis = 2;
+            else if(l_pos == 7) l_dis = 1;
+            else if(l_pos == 2) l_dis = 2;
+            else if(l_pos == 5) l_dis = 1;
+            else if(l_pos == 8) l_dis = 0;
+            else if(l_pos == 0) l_dis = 1;
             
             if(r_pos == 3) r_dis = 3;
-            if(r_pos == 6) r_dis = 2;
-            if(r_pos == 9) r_dis = 1;
-            if(r_pos == 2) r_dis = 2;
-            if(r_pos == 5) r_dis = 1;
-            if(r_pos == 8) r_dis = 0;
-            if(r_pos == 0) r_dis = 1;
+            else if(r_pos == 6) r_dis = 2;
+            else if(r_pos == 9) r_dis = 1;
+            else if(r_pos == 2) r_dis = 2;
+            else if(r_pos == 5) r_dis = 1;
+            else if(r_pos == 8) r_dis = 0;
+            else if(r_pos == 0) r_dis = 1;
             
             if(l_dis > r_dis) {answer.append("R"); r_pos = *iter;}
             else if(l_dis < r_dis) {answer.append("L"); l_pos = *iter;}
@@ -95,20 +95,20 @@ string solution(vector<int> numbers, string hand) {
         }
         else if(*iter == 0){
             if(l_pos == 1) l_dis = 4;
-            if(l_pos == 4) l_dis = 3;
-            if(l_pos == 7) l_dis = 2;
-            if(l_pos == 2) l_dis = 3;
-            if(l_pos == 5) l_dis = 2;
-            if(l_pos == 8) l_dis = 1;
-            if(l_pos == 0) l_dis = 0;
+            else if(l_pos == 4) l_dis = 3;
+            else if(l_pos == 7) l_dis = 2;
+            else if(l_pos == 2) l_dis = 3;
+            else if(l_pos == 5) l_dis = 2;
+            else if(l_pos == 8) l_dis = 1;
+            else if(l_pos == 0) l_dis = 0;
             
             if(r_pos == 3) r_dis = 4;
-            if(r_pos == 6) r_dis = 3;
-            if(r_pos == 9) r_dis = 2;
-            if(r_pos == 2) r_dis = 3;
-            if(r_pos == 5) r_dis = 2;
-            if(r_pos == 8) r_dis = 1;
-            if(r_pos == 0) r_dis = 0;
+            else if(r_pos == 6) r_dis = 3;
+            else if(r_pos == 9) r_dis = 2;
+            else if(r_pos == 2) r_dis = 3;
+            else if(r_pos == 5) r_dis = 2;
+            else if(r_pos == 8) r_dis = 1;
+            else if(r_pos == 0) r_dis = 0;
             
             if(l_dis > r_dis) {answer.append("R"); r_pos = *iter;}
             else if(l_dis < r_dis) {answer.append("L"); l_pos = *iter;}
